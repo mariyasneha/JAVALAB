7d14e2cf85e40dd8c6ea27ab678feac26ebc8c35
mariyasneha <153752523+mariyasneha@users.noreply.github.com>
2025-04-08 12:26:55 +0530

    Update shapes.java

--- shapes.java
+++ shapes.java
@@ -37,20 +37,38 @@ class rectangle implements shape{
 public class shapes{
 	public static void main(String[] args){
 		Scanner sc=new Scanner(System.in);
-		System.out.println("Enter the radius of circle:");
-		int r=sc.nextInt();
-		circle c=new circle(r);
-		c.area();
-		c.perimeter();
-		System.out.println("Enter length of rectangle:");
-		int l=sc.nextInt();
-		System.out.println("Enter breadth of rectangle:");
-		int b=sc.nextInt();
-		rectangle rect=new rectangle(l,b);
-		rect.area();
-		rect.perimeter();
+		int choice;
+		do{
+			System.out.println("\n=== Menu ===");
+            System.out.println("1. Calculate area and perimeter of a circle");
+            System.out.println("2. Calculate area and perimeter of a rectangle");
+            System.out.println("3. Exit");
+            System.out.print("Enter your choice: ");
+            choice = sc.nextInt();
+            switch(choice){
+            case 1:
+				System.out.println("Enter the radius of circle:");
+				int r=sc.nextInt();
+				circle cir=new circle(r);
+				cir.area();
+				cir.perimeter();
+				break;
+			case 2:
+				System.out.println("Enter length of rectangle:");
+				int l=sc.nextInt();
+				System.out.println("Enter breadth of rectangle:");
+				int b=sc.nextInt();
+				rectangle rect=new rectangle(l,b);
+				rect.area();
+				rect.perimeter();
+				break;
+			case 3:
+				System.out.println("Exit");
+				break;
+			default:
+				System.out.println("Invalid choice");
+			}
+		}while(choice !=3);
 		sc.close();
 	}
 }
-		
-			
